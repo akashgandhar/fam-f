db9d50b4e813f1ec79dd3d74c804b6af85e0becf
akashgandhar <akashgandhar007@gmail.com>
2024-10-13 13:14:00 +0530

    v1 api

--- src/redux/axios/apiCall.js
+++ src/redux/axios/apiCall.js
@@ -5,54 +5,54 @@ const header = {
   "Content-Type": "application/json",
 };
 export const LoginWithPhone = async (data) => {
-  const response = await authAxios.post("user/login", data, {
+  const response = await authAxios.post("v1/api/user/login", data, {
     headers: header,
   });
   return response.data;
 };
 
 export const VerifyLoginPhone = async (data) => {
-  const response = await authAxios.post("user/verify-login", data, {
+  const response = await authAxios.post("v1/api/user/verify-login", data, {
     headers: header,
   });
   return response.data;
 };
 export const UpdateUserApi = async (data) => {
-  const response = await authAxios.put("user/update/profile", data, {
+  const response = await authAxios.put("v1/api/user/update/profile", data, {
     headers: header,
   });
   return response.data;
 };
 export const LoginApi = async (data) => {
-  const response = await authAxios.post("user/social-login", data, {
+  const response = await authAxios.post("v1/api/user/social-login", data, {
     headers: header,
   });
   return response.data;
 };
 
 export const GetOrderListApi = async () => {
-  const response = await authAxios.post("user/order/list", {
+  const response = await authAxios.post("v1/api/user/order/list", {
     headers: header,
   });
   return response.data;
 };
 
 export const GetGiftListApi = async () => {
-  const response = await authAxios.post("user/order/promo/list", {
+  const response = await authAxios.post("v1/api/user/order/promo/list", {
     headers: header,
   });
   return response.data;
 };
 
 export const GetGiftCardApi = async () => {
-  const response = await authAxios.get("user/promo/list", {
+  const response = await authAxios.get("v1/api/user/promo/list", {
     headers: header,
   });
   return response.data;
 };
 
 export const GetWebsiteDataApi = async () => {
-  const response = await authAxios.get("user/website", {
+  const response = await authAxios.get("v1/api/user/website", {
     headers: header,
   });
   console.log(response,"res");
@@ -60,21 +60,21 @@ export const GetWebsiteDataApi = async () => {
 };
 
 export const GetAddressApi = async () => {
-  const response = await authAxios.get("user/address/view", {
+  const response = await authAxios.get("v1/api/user/address/view", {
     headers: header,
   });
   return response.data;
 };
 
 export const BuyGiftCardApi = async (data) => {
-  const response = await authAxios.post("user/promo/buy", data, {
+  const response = await authAxios.post("v1/api/user/promo/buy", data, {
     headers: header,
   });
   return response.data;
 };
 
 // export const CheckPriceApi = async (data) => {
-//   const response = await authAxios.post("user/order/cost", data, {
+//   const response = await authAxios.post("v1/api/user/order/cost", data, {
 //     headers: header,
 //   });
 //   return response.data;
@@ -87,28 +87,28 @@ export const CheckPriceApi = async (data) => {
 };
 
 export const UpdateAddressApi = async (data) => {
-  const response = await authAxios.put("user/address/edit", data, {
+  const response = await authAxios.put("v1/api/user/address/edit", data, {
     headers: header,
   });
   return response.data;
 };
 
 export const contactUsApi = async (data) => {
-  const response = await authAxios.post("user/contact", data, {
+  const response = await authAxios.post("v1/api/user/contact", data, {
     headers: header,
   });
   return response.data;
 };
 
 export const sendOTPApi = async (data) => {
-  const response = await authAxios.post("user/otp/send", data, {
+  const response = await authAxios.post("v1/api/user/otp/send", data, {
     headers: header,
   });
   return response.data;
 };
 
 export const verifyOTPApi = async (data) => {
-  const response = await authAxios.post("user/otp/verify", data, {
+  const response = await authAxios.post("v1/api/user/otp/verify", data, {
     headers: header,
   });
   return response.data;
@@ -116,7 +116,7 @@ export const verifyOTPApi = async (data) => {
 
 export const DownloadInvoiceSection = async (data) => {
   try {
-    const response = await authAxios.post("user/order/invoice", data, {
+    const response = await authAxios.post("v1/api/user/order/invoice", data, {
       headers: header,
     });
 
